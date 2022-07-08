c0af5a7c5c5abdd14b73d77fe7be6ff066170e1e
mahmood wirya <mahmudhaji28@yahoo.com>
2022-07-08 12:47:39 +0300

    project 1 done

--- project1/script.js
+++ project1/script.js
@@ -1,27 +1,47 @@
-function resize(){
-    let url = Document.getElementById('url').value;
-    let width = Document.getElementById('width').value;
-    let height = Document.getElementById('height').value;
-
-    let lastindex = url.lastIndexOf('/');
-    
-    let lastpart = url.substring(lastindex);
-    // let generatedURL =`https://source.unsplash.com${lastPart}/${width}x${height}`;
-    
-    let generatedURL = "https://source.unsplash.com"+lastpart+"/"+width+"x"+height;
-    
-    getElementById("UrlWithText").innerHTML=generatedURL;
-//     let myImage=getElementById('myImage');
-//     let myImageString = string(generatedURL);
-//     myImage.src= myImageString;
-//     myImage.style.display='block';
-    
-//  console.log(generatedURL);
-    
-    
-
-    // alert(generatedURL);
-}
-
-//unsplash.com/photos/RfoISVdKM4U
-//source.unsplash.com/RfoISVdKM4U/widthxheight
+function resize() {
+    let url = document.getElementById('url').value;
+    let height = document.getElementById('height').value;
+    let width = document.getElementById('width').value;
+  
+    /* only returns index number of last occurence of the forward slash : 27 */
+    let lastIndex = url.lastIndexOf('/');
+    //
+    let lastPart = url.substring(lastIndex);
+    let generatedUrl = `https://source.unsplash.com${lastPart}/${width}x${height}`;
+  
+    // alert(generatedUrl);
+  
+    // https://unsplash.com/photos/mASiL-TP0eU
+    // https://source.unsplash.com/mASiL-TP0eU/heightxwidth
+  
+    /* Make Image Visible Once We Have The URL Generated */
+    let ourImage = document.getElementById('ourImage');
+    ourImage.src = `${generatedUrl}`;
+    ourImage.style.display = 'block';
+  
+    /* Make URL Placeholder appear once we have the URL */
+    let urlDiv = document.getElementById('new-url');
+  
+    urlDiv.style.display = 'inline-block';
+    urlDiv.innerHTML = `<i class="bi bi-clipboard"></i>  ${generatedUrl}`;
+  }
+  
+  function handleCopy() {
+    // retrieve the text from inside the element itself
+    //  copy that text to clipboard
+    let innerTxt = document.getElementById('new-url').innerText;
+  
+    copyToClipboard(innerTxt);
+  }
+  
+  /* copy whatever passed to it to the clipboard */
+  function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(
+      function () {
+        console.log('Async: Copying to clipboard was successful!');
+      },
+      function (err) {
+        console.error('Async: Could not copy text: ', err);
+      }
+    );
+  }
